9a201ecd620c4ebb4af8d6bae077780515a616a6
Marlon Stoops <marlon.stoops@hotmail.com>
2020-10-21 18:53:07 +0200

    Add autotyper

--- roles/zsh/files/alias.zsh
+++ roles/zsh/files/alias.zsh
@@ -1,2 +1,3 @@
 alias jelasticssh='ssh 1127@gate.cloud.interhostsolutions.be -p 3022'
 alias cdr='cd $(git rev-parse --show-toplevel)'
+alias autotype='sleep 2.0; xdotool type "$(xclip -o -selection clipboard)"'
